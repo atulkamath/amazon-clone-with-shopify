d4fa9eeac0e4a4b87ab9c60ed300614e4ee1f2b1
atulkamath <realatulkamath@gmail.com>
2021-11-25 11:54:08 +0400

    Hero Complete

---
+++ components/large-carousel.js
@@ -0,0 +1,61 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import Image from 'next/image'
+import React, { useState } from 'react'
+const LargeCarousel = () => {
+  const largeImage = [
+    'https://m.media-amazon.com/images/I/71gs3Tx7IPL._SX3000_.jpg',
+    'https://m.media-amazon.com/images/I/71nw3LjRK3L._SX3000_.jpg',
+    'https://m.media-amazon.com/images/I/71HZnvHVBYL._SX3000_.jpg',
+    'https://m.media-amazon.com/images/I/71JS6GUuXoL._SX3000_.jpg',
+  ]
+
+  const [firstIndex, setFirstIndex] = useState(0)
+  const [lastIndex, setLastIndex] = useState(1)
+
+  return (
+    <div className={'hidden overflow-x-scroll sm:flex'}>
+      {firstIndex > 0 && (
+        <div
+          className=""
+          onClick={() => {
+            setFirstIndex(firstIndex - 1)
+            setLastIndex(lastIndex - 1)
+          }}
+        >
+          <button>
+            <ChevronLeftIcon className="absolute z-20 h-20 text-white w-14 left-16 top-52" />
+          </button>
+        </div>
+      )}
+      {lastIndex < largeImage.length && (
+        <div
+          onClick={() => {
+            setFirstIndex(firstIndex + 1)
+            setLastIndex(lastIndex + 1)
+          }}
+        >
+          <button>
+            <ChevronRightIcon className="absolute z-20 h-20 text-white w-14 top-52 right-16" />
+          </button>
+        </div>
+      )}
+      {largeImage.map((item, i) => {
+        if (i >= firstIndex && i < lastIndex) {
+          return (
+            <Image
+              width={1440}
+              height={600}
+              src={item}
+              layout={'fixed'}
+              alt="hero image"
+            />
+          )
+        } else {
+          return null
+        }
+      })}
+    </div>
+  )
+}
+
+export default LargeCarousel
--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -1,31 +1,61 @@
-import React, { FC } from 'react'
-import { Container } from '@components/ui'
-import { ArrowRight } from '@components/icons'
-import s from './Hero.module.css'
-import Link from 'next/link'
+import LargeCarousel from '@components/large-carousel'
+import TileCarousel from '@components/tile-carousel'
+import Image from 'next/image'
+import React, { FC, useEffect, useState } from 'react'
+
 interface HeroProps {
   className?: string
   headline: string
   description: string
+  data: object
 }
 
-const Hero: FC<HeroProps> = ({ headline, description }) => {
+const smallImage = [
+  'https://m.media-amazon.com/images/I/71Fia1VO1PL._SR1236,1080_.jpg',
+  'https://m.media-amazon.com/images/I/714I3Yg0C3L._SR1236,1080_.jpg',
+  'https://m.media-amazon.com/images/I/61Jhrv+qH-L._SR1236,1080_.jpg',
+  'https://m.media-amazon.com/images/I/71DmEEOsz9L._SR1236,1080_.jpg',
+]
+
+const Hero: FC<HeroProps> = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentIndex === smallImage.length - 1) {
+        setCurrentIndex(0)
+      } else {
+        setCurrentIndex(currentIndex + 1)
+      }
+    }, 3000)
+    return () => clearInterval(intervalId)
+  }, [currentIndex])
+
   return (
-    <div className="bg-accent-9 border-b border-t border-accent-2">
-      <Container>
-        <div className={s.root}>
-          <h2 className={s.title}>{headline}</h2>
-          <div className={s.description}>
-            <p>{description}</p>
-            <Link href="/">
-              <a className="flex items-center text-accent-0 pt-3 font-bold hover:underline cursor-pointer w-max-content">
-                Read it here
-                <ArrowRight width="20" heigh="20" className="ml-1" />
-              </a>
-            </Link>
-          </div>
-        </div>
-      </Container>
+    <div className="bg-accent-1 ">
+      <LargeCarousel />
+      <div className="overflow-hidden sm:hidden">
+        <Image
+          width={400}
+          height={350}
+          src={smallImage[currentIndex]}
+          layout={'responsive'}
+          alt="hero image"
+        />
+      </div>
+      <TileCarousel />
+      <section className="lg:hidden">
+        <Image
+          width={'100vw'}
+          height={20}
+          objectFit="contain"
+          src={
+            'https://m.media-amazon.com/images/I/5160YmPsNLL._SR1242,150_.jpg'
+          }
+          alt=""
+          layout="responsive"
+        />
+      </section>
     </div>
   )
 }
