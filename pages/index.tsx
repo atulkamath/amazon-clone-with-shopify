308db0e8f921bb09eb8922e9a3d5d71d2e634ead
atulkamath <realatulkamath@gmail.com>
2021-11-25 12:10:17 +0400

    Typescript bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,10 +42,7 @@ export default function Home({
   return (
     <>
       <RoundCarousel />
-      <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
-      />
+      <Hero />
       <Grid variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
           <ProductCard
