894174d29982aa40ac7ee9e436c4da0f5d30c428
atulkamath <realatulkamath@gmail.com>
2021-12-04 18:43:31 +0400

    Carousel multi direction function

--- components/test-carousel.js
+++ components/test-carousel.js
@@ -9,23 +9,49 @@ const TestCarousel = () => {
     'https://m.media-amazon.com/images/I/71HZnvHVBYL._SX3000_.jpg',
     'https://m.media-amazon.com/images/I/71JS6GUuXoL._SX3000_.jpg',
   ]
-  const rightButton = () => {
-    setRight(true)
-    setCount(count++)
-  }
-  const [firstIndex, setFirstIndex] = useState(0)
-  const [lastIndex, setLastIndex] = useState(1)
+
+  const [left, setLeft] = useState(false)
   const [right, setRight] = useState(false)
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(0)
+  const [leftCount, setLeftCount] = useState(4)
+
+  const leftButton = () => {
+    console.log(leftCount)
+    setLeft(true)
+    setLeftCount(leftCount + 1)
+
+    if (leftCount > 5) {
+      setLeftCount(4)
+    }
+  }
+  const rightButton = () => {
+    console.log(count)
+    setRight(true)
+    setCount(count + 1)
+
+    if (count > 2) {
+      setCount(0)
+    }
+  }
   return (
-    <div className="flex w-full ">
-      <button className="absolute z-20 top-20" onClick={rightButton}>
-        Right
+    <div className="hidden w-full sm:flex ">
+      <button
+        className="absolute z-20 items-center justify-center top-1/4 left-10"
+        onClick={leftButton}
+      >
+        <ChevronLeftIcon className="w-24 h-16" />
       </button>
+      <button
+        className="absolute z-20 items-center justify-center top-1/4 right-10"
+        onClick={rightButton}
+      >
+        <ChevronRightIcon className="w-24 h-16" />
+      </button>
+
       {largeImage.map((item, index) => (
         <div
           className={
-            right
+            right || left
               ? `transform translate-x-count${count} transition duration-500`
               : ''
           }
