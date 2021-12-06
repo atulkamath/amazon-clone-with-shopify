ff26c7f878a03ecb5f15248b44c09030f0ceb3e7
atulkamath <realatulkamath@gmail.com>
2021-12-07 01:52:07 +0400

    changes to tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ module.exports = {
   },
 
   purge: {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
+    options: {
+      safelist: {
+        standard: ['outline-none'],
+      },
+    },
   },
   theme: {
     extend: {
