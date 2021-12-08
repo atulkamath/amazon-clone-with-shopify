06a494d9ab3bfcc610e5091060323572c5e689a7
atulkamath <realatulkamath@gmail.com>
2021-12-08 12:59:04 +0400

    cart sidebar ready.

--- components/cart/CartSidebarView/CartSidebarView.tsx
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -116,7 +116,7 @@ const CartSidebarView: FC = () => {
                   Proceed to Checkout ({total})
                 </Button>
               ) : (
-                <Button href="/checkout" Component="a" width="100%">
+                <Button href="/cart" Component="a" width="100%">
                   Proceed to Checkout
                 </Button>
               )}
