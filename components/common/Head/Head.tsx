ae4173b908d47b407f0f52c7d821d79cda7f1941
atulkamath <realatulkamath@gmail.com>
2021-12-08 15:57:48 +0400

    Fixed favicon and disbaled zoom on mobile

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -10,6 +10,12 @@ const Head: FC = () => {
       <NextHead>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+        <link rel="icon" type="image/png" href="/favicon_small.jpeg" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta name="HandheldFriendly" content="true" />
       </NextHead>
     </>
   )
