e6a9c68a55f5470b1b884254fa278eb3fb9500c1
atulkamath <realatulkamath@gmail.com>
2021-12-08 16:43:22 +0400

    page ready for mobile

--- components/checkout/CheckoutSidebarView/CheckoutSidebarView.tsx
+++ components/checkout/CheckoutSidebarView/CheckoutSidebarView.tsx
@@ -42,7 +42,7 @@ const CheckoutSidebarView: FC = () => {
       className={s.root}
       handleBack={() => setSidebarView('CART_VIEW')}
     >
-      <div className="px-4 sm:px-6 flex-1">
+      <div className="flex-1 px-4 sm:px-6">
         <Link href="/cart">
           <a>
             <Text variant="sectionHeading">Checkout</Text>
@@ -72,7 +72,7 @@ const CheckoutSidebarView: FC = () => {
 
       <form
         onSubmit={handleSubmit}
-        className="flex-shrink-0 px-6 py-6 sm:px-6 sticky z-20 bottom-0 w-full right-0 left-0 bg-accent-0 border-t text-sm"
+        className="sticky bottom-0 left-0 right-0 z-20 flex-shrink-0 w-full px-6 py-6 text-sm border-t sm:px-6 bg-accent-0"
       >
         <ul className="pb-2">
           <li className="flex justify-between py-1">
@@ -88,7 +88,7 @@ const CheckoutSidebarView: FC = () => {
             <span className="font-bold tracking-wide">FREE</span>
           </li>
         </ul>
-        <div className="flex justify-between border-t border-accent-2 py-3 font-bold mb-2">
+        <div className="flex justify-between py-3 mb-2 font-bold border-t border-accent-2">
           <span>Total</span>
           <span>{total}</span>
         </div>
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -41,23 +41,23 @@ export default function Cart() {
   )
 
   return (
-    <div className="grid lg:grid-cols-12 w-full max-w-7xl mx-auto">
+    <div className="grid w-full mx-auto lg:grid-cols-12 max-w-7xl">
       <div className="lg:col-span-8">
         {isLoading || isEmpty ? (
-          <div className="flex-1 px-12 py-24 flex flex-col justify-center items-center ">
-            <span className="border border-dashed border-secondary flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary">
+          <div className="flex flex-col items-center justify-center flex-1 px-12 py-24 ">
+            <span className="flex items-center justify-center w-16 h-16 p-12 border border-dashed rounded-lg border-secondary bg-primary text-primary">
               <Bag className="absolute" />
             </span>
             <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
               Your cart is empty
             </h2>
-            <p className="text-accent-6 px-10 text-center pt-2">
+            <p className="px-10 pt-2 text-center text-accent-6">
               Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
             </p>
           </div>
         ) : error ? (
-          <div className="flex-1 px-4 flex flex-col justify-center items-center">
-            <span className="border border-white rounded-full flex items-center justify-center w-16 h-16">
+          <div className="flex flex-col items-center justify-center flex-1 px-4">
+            <span className="flex items-center justify-center w-16 h-16 border border-white rounded-full">
               <Cross width={24} height={24} />
             </span>
             <h2 className="pt-6 text-xl font-light text-center">
@@ -66,8 +66,8 @@ export default function Cart() {
             </h2>
           </div>
         ) : success ? (
-          <div className="flex-1 px-4 flex flex-col justify-center items-center">
-            <span className="border border-white rounded-full flex items-center justify-center w-16 h-16">
+          <div className="flex flex-col items-center justify-center flex-1 px-4">
+            <span className="flex items-center justify-center w-16 h-16 border border-white rounded-full">
               <Check />
             </span>
             <h2 className="pt-6 text-xl font-light text-center">
@@ -75,10 +75,47 @@ export default function Cart() {
             </h2>
           </div>
         ) : (
-          <div className="px-4 sm:px-6 flex-1">
-            <Text variant="pageHeading">My Cart</Text>
+          <div className="flex-1 px-4 sm:px-6">
+            <ul className="py-3">
+              <li className="flex justify-between py-1">
+                <span>Subtotal</span>
+                <span>{subTotal}</span>
+              </li>
+              <li className="flex justify-between py-1">
+                <span>Taxes</span>
+                <span>Calculated at checkout</span>
+              </li>
+              <li className="flex justify-between py-1">
+                <span>Estimated Shipping</span>
+                <span className="font-bold tracking-wide">FREE</span>
+              </li>
+            </ul>
+
+            {/* <div className="flex justify-between py-3 mb-10 font-bold border-t border-accent-2">
+                <span>Total</span>
+                <span>{total}</span>
+              </div> */}
+            <div className="flex flex-row justify-end">
+              <div className="w-full lg:w-72">
+                {isEmpty ? (
+                  <Button href="/" Component="a" width="100%">
+                    Continue Shopping
+                  </Button>
+                ) : (
+                  <Button
+                    href="/checkout"
+                    Component="a"
+                    variant="slim"
+                    width="100%"
+                  >
+                    Proceed to Buy
+                  </Button>
+                )}
+              </div>
+            </div>
+
             <Text variant="sectionHeading">Review your Order</Text>
-            <ul className="py-6 space-y-6 sm:py-0 sm:space-y-0 sm:divide-y sm:divide-accent-2 border-b border-accent-2">
+            <ul className="py-6 space-y-6 border-b sm:py-0 sm:space-y-0 sm:divide-y sm:divide-accent-2 border-accent-2">
               {data!.lineItems.map((item: any) => (
                 <CartItem
                   key={item.id}
@@ -96,7 +133,7 @@ export default function Cart() {
                 {[1, 2, 3, 4, 5, 6].map((x) => (
                   <div
                     key={x}
-                    className="border border-accent-3 w-full h-24 bg-accent-2 bg-opacity-50 transform cursor-pointer hover:scale-110 duration-75"
+                    className="w-full h-24 duration-75 transform bg-opacity-50 border cursor-pointer border-accent-3 bg-accent-2 hover:scale-110"
                   />
                 ))}
               </div>
@@ -110,11 +147,11 @@ export default function Cart() {
             <>
               {/* Shipping Address */}
               {/* Only available with customCheckout set to true - Meaning that the provider does offer checkout functionality. */}
-              <div className="rounded-md border border-accent-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accent-4">
+              <div className="flex items-center justify-center px-6 py-6 mb-4 text-center border rounded-md cursor-pointer border-accent-2 hover:border-accent-4">
                 <div className="mr-5">
                   <MapPin />
                 </div>
-                <div className="text-sm text-center font-medium">
+                <div className="text-sm font-medium text-center">
                   <span className="uppercase">+ Add Shipping Address</span>
                   {/* <span>
                     1046 Kearny Street.<br/>
@@ -124,50 +161,17 @@ export default function Cart() {
               </div>
               {/* Payment Method */}
               {/* Only available with customCheckout set to true - Meaning that the provider does offer checkout functionality. */}
-              <div className="rounded-md border border-accent-2 px-6 py-6 mb-4 text-center flex items-center justify-center cursor-pointer hover:border-accent-4">
+              <div className="flex items-center justify-center px-6 py-6 mb-4 text-center border rounded-md cursor-pointer border-accent-2 hover:border-accent-4">
                 <div className="mr-5">
                   <CreditCard />
                 </div>
-                <div className="text-sm text-center font-medium">
+                <div className="text-sm font-medium text-center">
                   <span className="uppercase">+ Add Payment Method</span>
                   {/* <span>VISA #### #### #### 2345</span> */}
                 </div>
               </div>
             </>
           )}
-          <div className="border-t border-accent-2">
-            <ul className="py-3">
-              <li className="flex justify-between py-1">
-                <span>Subtotal</span>
-                <span>{subTotal}</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Taxes</span>
-                <span>Calculated at checkout</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Estimated Shipping</span>
-                <span className="font-bold tracking-wide">FREE</span>
-              </li>
-            </ul>
-            <div className="flex justify-between border-t border-accent-2 py-3 font-bold mb-10">
-              <span>Total</span>
-              <span>{total}</span>
-            </div>
-          </div>
-          <div className="flex flex-row justify-end">
-            <div className="w-full lg:w-72">
-              {isEmpty ? (
-                <Button href="/" Component="a" width="100%">
-                  Continue Shopping
-                </Button>
-              ) : (
-                <Button href="/checkout" Component="a" width="100%">
-                  Proceed to Checkout
-                </Button>
-              )}
-            </div>
-          </div>
         </div>
       </div>
     </div>
