5cbc0f1fe17a278bf6b50b7ac92bfb53630aee30
atulkamath <realatulkamath@gmail.com>
2022-01-13 10:35:56 +0400

    added additional tile

--- components/tile-carousel.js
+++ components/tile-carousel.js
@@ -17,7 +17,10 @@ const tileData = [
     name: 'https://m.media-amazon.com/images/I/51-+6nr0bDL._SR270,360_.jpg',
     link: '/clothing',
   },
-
+  {
+    name: ' https://m.media-amazon.com/images/I/41zcgAO4k0L._SR270,360_.jpg',
+    link: '/electronics',
+  },
   {
     name: 'https://m.media-amazon.com/images/I/51eiFnOS0PL._SR270,360_.jpg',
     link: '/clothing',
